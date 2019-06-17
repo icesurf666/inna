c43c72ffe5a51e5e0f3597a3692bf0cb331edc6f
icesurf666 <icesurf@yandex.ru>
2019-06-17 18:36:46 +0600

    first commit

---
+++ src/countries.js
@@ -0,0 +1,280 @@
+
+const select = [
+    {
+        name: 'Австрия',
+        code: 'AT',
+        selected: false
+    },
+    {
+        name: 'Болгария',
+        code: 'BG',
+        selected: false
+    },
+    {
+        name: 'Великобритания',
+        code: 'GB',
+        selected: false
+    },
+    {
+        name: 'Венгрия',
+        code: 'HU',
+        selected: false
+    },
+    {
+        name: 'Германия',
+        code: 'DE',
+        selected: false
+    },
+    {
+        name: 'Греция',
+        code: 'GR',
+        selected: false
+    },
+    {
+        name: 'Ирландия',
+        code: 'IE',
+        selected: false
+    },
+    {
+        name: 'Испания',
+        code: 'ES',
+        selected: false
+    },
+    {
+        name: 'Италия',
+        code: 'IT',
+        selected: false
+    },
+    {
+        name: 'Кипр',
+        code: 'CY',
+        selected: false
+    },
+    {
+        name: 'Литва',
+        code: 'LT',
+        selected: false
+    },
+    {
+        name: 'Польша',
+        code: 'PL',
+        selected: false
+    },
+    {
+        name: 'Португалия',
+        code: 'PT',
+        selected: false
+    },
+    {
+        name: 'Румыния',
+        code: 'RO',
+        selected: false
+    },
+    {
+        name: 'Слования',
+        code: 'SK',
+        selected: false
+    },
+    {
+        name: 'Турция',
+        code: 'TR',
+        selected: false
+    },
+    {
+        name: 'Франция',
+        code: 'FR',
+        selected: false
+    },
+    {
+        name: 'Черногория',
+        code: 'ME',
+        selected: false
+    },
+    {
+        name: 'Чехия',
+        code: 'CZ',
+        selected: false
+    },
+    {
+        name: 'Швейцария',
+        code: 'CH',
+        selected: false
+    },
+    {
+        name: 'Израиль',
+        code: 'IL',
+        selected: false
+    },
+    {
+        name: 'Индия',
+        code: 'IN',
+        selected: false
+    },
+    {
+        name: 'Индонезия',
+        code: 'ID',
+        selected: false
+    },
+    {
+        name: 'Камбоджа',
+        code: 'KH',
+        selected: false
+    },
+    {
+        name: 'Китай',
+        code: 'CN',
+        selected: false
+    },
+    {
+        name: 'ОАЭ',
+        code: 'AE',
+        selected: false
+    },
+    {
+        name: 'Сингапур',
+        code: 'SG',
+        selected: false
+    },
+    {
+        name: 'Таиланд',
+        code: 'TH',
+        selected: false
+    },
+    {
+        name: 'Филиппины',
+        code: 'PH',
+        selected: false
+    },
+    {
+        name: 'Шри-Ланка',
+        code: 'LK',
+        selected: false
+    },
+    {
+        name: 'Япония',
+        code: 'JP',
+        selected: false
+    },
+    {
+        name: 'Ботсвана',
+        code: 'BW',
+        selected: false
+    },
+    {
+        name: 'Египет',
+        code: 'EG',
+        selected: false
+    },
+    {
+        name: 'Кения',
+        code: 'KE',
+        selected: false
+    },
+    {
+        name: 'Марокко',
+        code: 'MA',
+        selected: false
+    },
+    {
+        name: 'Намибия',
+        code: 'NA',
+        selected: false
+    },
+    {
+        name: 'Сейшелы',
+        code: 'SC',
+        selected: false
+    },
+    {
+        name: 'Танзания',
+        code: 'TZ',
+        selected: false
+    },
+    {
+        name: 'Тунис',
+        code: 'TN',
+        selected: false
+    },
+    {
+        name: 'ЮАР',
+        code: 'ZA',
+        selected: false
+    },
+    {
+        name: 'Доминиканская республика',
+        code: 'DO',
+        selected: false
+    },
+    {
+        name: 'Коста-Рика',
+        code: 'CR',
+        selected: false
+    },
+    {
+        name: 'Куба',
+        code: 'CU',
+        selected: false
+    },
+    {
+        name: 'Мексика',
+        code: 'MX',
+        selected: false
+    },
+    {
+        name: 'Панама',
+        code: 'PA',
+        selected: false
+    },
+    {
+        name: 'США',
+        code: 'US',
+        selected: false
+    },
+    {
+        name: 'Ямайка',
+        code: 'JM',
+        selected: false
+    },
+    {
+        name: 'Аргентина',
+        code: 'AR',
+        selected: false
+    },
+    {
+        name: 'Бразилия',
+        code: 'BR',
+        selected: false
+    },
+    {
+        name: 'Венесуэла',
+        code: 'VE',
+        selected: false
+    },
+    {
+        name: 'Перу',
+        code: 'PE',
+        selected: false
+    },
+    {
+        name: 'Чили',
+        code: 'CL',
+        selected: false
+    },
+    {
+        name: 'Эквадор',
+        code: 'EC',
+        selected: false
+    },
+    {
+        name: 'Австралия',
+        code: 'AU',
+        selected: false
+    },
+    {
+        name: 'Новая Зеландия',
+        code: 'NZ',
+        selected: false
+    }
+];
+
+console.log(select.length);
